3b67d82ddb6d5634403ecdf02eeb327ca31b389c
Baptiste Dellarocca <bdellaro@c1r1p8.42lehavre.fr>
2023-12-19 14:42:58 +0100

    Last before eval, retry 4

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: bdellaro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 11:26:54 by bdellaro          #+#    #+#             */
-/*   Updated: 2023/12/12 11:27:07 by bdellaro         ###   ########.fr       */
+/*   Updated: 2023/12/19 14:39:55 by bdellaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
@@ -88,10 +88,10 @@ char	*get_next_line(int fd)
 	free(buffer);
 	return (ft_print_line(&str));
 }
-/*
+
 int	main(void)
 {
-	int fd = open("../File_descriptori.txt", O_RDONLY);
+	int fd = open("../File_descriptor.txt", O_RDONLY);
 	char	*str;
 	
 	while (1)
@@ -104,4 +104,4 @@ int	main(void)
 	}
 	close(fd);
 	return (0);
-}*/
+}
