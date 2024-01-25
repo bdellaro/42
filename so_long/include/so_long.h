902854fe5cd047727a6a4430eea2637df6fc4ef0
Baptiste Dellarocca <bdellaro@c2r12p5.42lehavre.fr>
2024-01-25 19:16:09 +0100

    solong background OK

--- so_long/include/so_long.h
+++ so_long/include/so_long.h
@@ -1,7 +1,7 @@
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# include "../minilibx-linux/mlx.h"
+# include "../MLX42/mlx42.h"
 # include "../libft/libft.h"
 # include <stdlib.h>
 # include <stdio.h>
@@ -15,39 +15,32 @@
 
 # define MLX_ERROR -1
 
-# define BLACK       0x000000  // RGB(0, 0, 0)
-# define WHITE       0xFFFFFF  // RGB(255, 255, 255)
-# define RED         0xFF0000  // RGB(255, 0, 0)
-# define GREEN       0x00FF00  // RGB(0, 255, 0)
-# define BLUE        0x0000FF  // RGB(0, 0, 255)
-
-# define MAGENTA_BURST   0xFF00FF  
-# define LIME_SHOCK      0xCCFF00  
-# define NEON_ORANGE     0xFF6600  
-# define PSYCHEDELIC_PURPLE 0x660066  
-# define AQUA_DREAM      0x33CCCC  
-# define HOT_PINK        0xFF66B2 
-# define ELECTRIC_BLUE   0x0066FF 
-# define LAVA_RED        0xFF3300 
-
-typedef struct	s_data
+typedef struct s_instance
 {
-	void	*img_ptr;
-	char		*address;
-	int			bits_per_pixel;
-	int			endian;
-	int			line_len;
-}			t_data;
+	int	x;
+	int	y;
+}			t_instance;
+
+/*typedef struct	s_image
+{
+	int	width;
+	int	height;
+	int		pixels;
+	instance_t	*instance;
+	int	count
+	bool	enabled;
+	void	*context;
+}			t_image;*/
 
 typedef struct	s_solong
 {
-	void	*mlx_ptr;
-	void	*win_ptr;
+	mlx_t		*mlx;
+	mlx_texture_t	*texture;
 	char		*key_pressed;
 	char		*name;
 	char		*arg;
 	int			color;
-	t_data	img;
+	mlx_image_t	*img;
 }			t_solong;
 
 int		ft_put_key(int keysym, t_solong *solong);
@@ -56,6 +49,5 @@ int		ft_handle_input(int keysym, t_solong *solong);
 void	ft_arguments_check(int argc, char **argv);
 void	ft_solong_init(t_solong *solong);
 void	ft_mlx_exit(t_solong *solong);
-void	my_pixel_put(t_data *img, int x, int y, int color);
 
 #endif
--- so_long/src/ft_main.c
+++ so_long/src/ft_main.c
@@ -6,68 +6,17 @@
 /*   By: bdellaro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 16:53:07 by bdellaro          #+#    #+#             */
-/*   Updated: 2024/01/18 17:51:06 by bdellaro         ###   ########.fr       */
+/*   Updated: 2024/01/25 18:09:54 by bdellaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../include/so_long.h"
 
-void	ft_color_check(char **argv)
-{
-	t_solong	solong;
-	int	color;
-	char	*upper;
-	int	i;
-
-	upper = (char *)malloc((ft_strlen(argv[1]) + 1) * sizeof(char));
-	i = 0;
-	while (argv[1][i])
-	{
-		upper[i] = ft_toupper(argv[1][i]);
-		i++;
-	}
-	upper[i] = 0;
-	color = -1;
-	solong.name = upper;
-	if (!ft_strncmp(upper, "RED", 3) && !ft_strncmp(upper, "RED",
-	4))
-		color = RED;
-	else if (!ft_strncmp(upper, "GREEN", 5) && !ft_strncmp(upper,
-	"GREEN", 6))
-		color = GREEN;
-	else if (!ft_strncmp(upper, "BLUE", 4) && !ft_strncmp(upper,
-	"BLUE", 5))
-		color = BLUE;
-	else if (!ft_strncmp(upper, "MAGENTA", 7) && !ft_strncmp(upper,
-	"MAGENTA", 8))
-		color = MAGENTA_BURST;
-	else if (!ft_strncmp(upper, "ORANGE", 6) && !ft_strncmp(upper,
-	"ORANGE", 7))
-		color = NEON_ORANGE;
-	else if (!ft_strncmp(upper, "PINK", 4) && !ft_strncmp(upper, "PINK", 5))
-		color = HOT_PINK;
-	else if (!ft_strncmp(upper, "WHITE", 5) && !ft_strncmp(upper,
-	"WHITE", 6))
-		color = WHITE;
-	else if (!ft_strncmp(upper, "BLACK", 5) && !ft_strncmp(upper, "BLACK", 6))
-		color = BLACK;
-	if (color != -1)
-	{
-		solong.color = color;
-		ft_printf("Color chosen : %s\n", upper);
-		ft_solong_init(&solong);
-	}
-	else
-	{
-		ft_printf("Invalid color name.\n");
-		exit (EXIT_FAILURE);
-	}
-	free(upper);
-}
-
 int	main(int argc, char **argv)
 {
+	t_solong	solong;
+	argv[1] = solong.name;
 	if (argc == 2)
-		ft_color_check(argv);
+		ft_solong_init(&solong);
 	else
 	{
 		ft_printf("Invalid argument(s).\n");
